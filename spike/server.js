f407be6a10e047cc26703929b54825b8804458d1
ShaeC12921 <sfclark2017@gmail.com>
2026-07-23 18:44:08 -0700

    basic socket emit functionality

--- spike/server.js
+++ spike/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
@@ -11,6 +11,12 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
     console.log("connected", socket.id);
     socket.on("disconnect", () => console.log("disconnected", socket.id));
+});
+
+io.on("connection", (socket) => {
+    socket.on("chat message", (msg) => {
+        console.log('message: ', msg);
+    });
 });
 
 httpServer.listen(3000, () => console.log("http://localhost:3000"));
