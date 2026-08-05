30f6e7bb1fcf2b557d6a6281e3256610eb5f9fd4
ShaeC12921 <sfclark2017@gmail.com>
2026-08-04 20:54:28 -0700

    3-way communicate truthfulness of rooms

--- spike/server.js
+++ spike/server.js
@@ -6,9 +6,14 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+const gameRooms = new Map();
 const userNames = new Map();
 const activeUsers = {};
 const buzzWinner = [];
+
+
+// TEST ENTRY
+gameRooms.set('AAAA')
 
 /*
 Need to build function that determines who clicked in first, 
@@ -52,12 +57,42 @@ io.on("connection", (socket) => {
         buzzWinner.length = 0;
     })
 
+    socket.on('join request', (roomCode) => {
+        io.emit('join request result', gameRooms.has(roomCode))
+        console.log(`${socket.id} tried using room code ${roomCode}. Result: ${gameRooms.has(roomCode)}`)
+    })
+
+    socket.on('room request', () => {
+        let roomCode = getStringOfFour();
+        while (gameRooms.has(roomCode)) {
+            roomCode = getStringOfFour();
+        }
+        gameRooms.set(roomCode)
+        console.log(`room request made, added ${roomCode} to roomMap`)
+        io.emit('room request confirm', roomCode);
+    })
+
     socket.on("disconnect", () => {
         console.log("disconnected", socket.id);
         userNames.delete(socket.id);
     })
     
-    
+
+
+
 });
+// Utility functions to generate room code
+const getRandomChar = () => {
+    const letter = String.fromCharCode(65 + Math.floor(Math.random() * 26))
+    return letter;
+}
+
+const getStringOfFour = () => {
+    let letters = '';
+    for (let i = 0; i < 4; i++) {
+        letters += getRandomChar()
+    }
+    return letters
+} 
 
 httpServer.listen(3000, () => console.log("http://localhost:3000"));
